de75d2de27d3f04ea38a512f7366721f7ae3e9d8
clemenceccmc <clemence.mucci_chiesa@edu.devinci.fr>
2026-02-19 12:50:17 +0100

    correction ajouter ligne

--- TourneeFutee/Program.cs
+++ TourneeFutee/Program.cs
@@ -5,6 +5,13 @@
         static void Main(string[] args)
         {
             // TODO : faire vos propres tests ici
+            Matrix m = new Matrix(nbRows: 2, nbColumns: 3, defaultValue: 0);
+            int nbRows = m.NbRows;
+            int nbColumns = m.NbColumns;
+
+            m.AddRow(0);
+            Console.WriteLine($"NbRows = {m.NbRows} (should be 3)");
+               
         }
     }
 }
